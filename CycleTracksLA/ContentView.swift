8253e079dd44fe842e91481d87e94a8a7194ad84
Horace Williams <horace.d.williams@gmail.com>
2019-11-04 18:50:17 -0800

    Record locations into an array while tracking

--- CycleTracksLA/ContentView.swift
+++ CycleTracksLA/ContentView.swift
@@ -10,52 +10,86 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    @Binding var locations: [MKUserLocation]
     func updateUIView(_ view: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(
-            latitude: 34.011286, longitude: -116.166868)
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        view.setRegion(region, animated: true)
+        view.setUserTrackingMode(.follow, animated: true)
     }
 
     typealias UIViewType = MKMapView
 
     func makeUIView(context: Context) -> MKMapView {
-        MKMapView(frame: .zero)
+        let view = MKMapView(frame: .zero)
+        view.delegate = context.coordinator
+        return view
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    final class Coordinator: NSObject, MKMapViewDelegate {
+        var control: MapView
+
+        init(_ control: MapView) {
+            self.control = control
+        }
+
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            print("*** updated user location")
+            print("\(userLocation)")
+            control.locations.append(userLocation)
+        }
+
+        func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+            print("view finished loading")
+            mapView.setUserTrackingMode(.follow, animated: true)
+            mapView.showsUserLocation = true
+        }
     }
 }
 
 struct MapView_Preview: PreviewProvider {
+    @State static var locations: [MKUserLocation] = []
     static var previews: some View {
-        MapView()
+        MapView(locations: $locations)
     }
 }
 
 struct RideView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-    let message: String
+    @State var locations: [MKUserLocation] = []
 
     var body: some View {
         VStack {
-            Text(message)
+            Text("Recording Ride. \(locations.count) Locations.")
             Button(action: {self.presentationMode.wrappedValue.dismiss()}) {
                 Text("dismiss")
             }
-            MapView()
+            MapView(locations: $locations)
         }
     }
 }
 
 struct ContentView: View {
     @State var rideModalExpanded = false
+    let locationManager = CLLocationManager()
+
+    func showRideModal() {
+        locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            self.rideModalExpanded = true
+        }
+    }
+
     var body: some View {
         VStack {
             Text("Hello: \(rideModalExpanded ? "yes" : "no")")
-            Button(action: {self.rideModalExpanded = true}) {
+            Button(action: self.showRideModal) {
                 Text("Record Ride")
             }
         }.sheet(isPresented: $rideModalExpanded, onDismiss: {print("dismissed")}) {
-            RideView(message: "Make a Ride")
+            RideView()
         }
     }
 }
